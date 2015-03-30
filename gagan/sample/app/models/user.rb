d1c8f3b7f6b67b8e97f77e9139cdfc3f1a0e6528
Gagandeep Arora <gagandeep.arora@letsgomo.com>
2015-03-30 18:47:38 +0530

    User Validation

--- gagan/sample/app/models/user.rb
+++ gagan/sample/app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
-	validates(:name, presence: true, length: { maximum: 50 } )
-	validates(:email, presence: true, length: { maximum: 255 } format: { with: /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i} )
+	before_save ( self.email = email.downcase )
+
+	validates :name, presence: true, length: { maximum: 50 } 
+
+	validates :email, presence: true, length: { maximum: 255 },
+						format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, 
+						niqueness: { case_sensitive: false} 
+
+	has_secure_password
+
+	validates :password, length: { minimum: 6} 
 end
--- gagan/sample/test/models/user_test.rb
+++ gagan/sample/test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 	def setup
-		@user = User.new(name: "Example User", email: "example@xyz.com")		
+		@user = User.new(name: "Example User", email: "example@xyz.com", 
+													password: "foo", password_confirmation: "foo")		
 	end
 
 	test "should be valid" do
@@ -30,7 +31,7 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "email validation should accept valid addresses" do
-		valid_addresses = %w[abc@xyz.com gagan_deep@xyz.COM gagan@xyz.com gagan_deep-arora@foo.bar.com foo.bar@xyz.net]
+		valid_addresses = %w[example@xyz.com gagan_deep@xyz.COM gagan@xyz.com gagan_deep-arora@foo.bar.com foo.bar@xyz.net]
 		valid_addresses.each do |valid_address|
 			@user.email = valid_address
 			assert @user.valid?, "#{valid_address.inspect} should be valid"
@@ -38,10 +39,29 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "email validation should reject invalid addresses" do
-		invalid_addresses = %w[abc@xyz,com abc_foo_bar.org abc@foo+bar.com abc.xyz@foo.bar@example.com]
+		invalid_addresses = %w[examp@xyz,com abc_foo_bar.org abc@foo+bar.com abc.xyz@foo.bar@example.com]
 		invalid_addresses.each do |invalid_address|
 			@user.email = invalid_address
-			assert @user.valid?, "#{invalid_address.inspect} should be invalid"
+			assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
 		end
+	end
+
+	test "email address should be unique" do
+		duplicate_user = @user.dup
+		duplicate_user.email = @user.email.upcase
+		@user.save
+		assert_not duplicate_user.valid?
+	end
+
+	test "email address should be saved in lower case" do
+		mixed_case_email = "FoO@Bar.CoM"
+		@user.email = mixed_case_email
+		@user.save2
+		assert_equal mixed_case_email.downcase, @user.reload.email
+	end
+
+	test "password should have minimum length" do
+		@user.password = @user.password_confirmation = "a" * 5
+		assert_not @user.valid?
 	end
 end
