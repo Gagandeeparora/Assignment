524fee9f21a45b35fb41d5dfc1353ec0654f856d
Gagandeep Arora <gagandeep.arora@letsgomo.com>
2015-04-03 17:08:04 +0530

    Sample App

--- gagan/sample/app/models/user.rb
+++ gagan/sample/app/models/user.rb
@@ -29,6 +29,15 @@ class User < ActiveRecord::Base
 
 	def remember
 		self.remember_token = User.new_token
-		update_attribute(:remember_digested, User.digest(remember_token))		
+		update_attribute(:remember_digest, User.digest(remember_token))		
+	end
+
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)		
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)		
 	end
 end
