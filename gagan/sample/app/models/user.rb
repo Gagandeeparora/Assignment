133acea98e9acfa073e3d5a6847ed583467e96fd
Gagandeep Arora <gagandeep.arora@letsgomo.com>
2015-04-03 11:04:53 +0530

    Sample App

--- gagan/sample/app/models/user.rb
+++ gagan/sample/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
 	has_secure_password
 
 	before_save :convert_email_to_downcase
@@ -20,5 +21,14 @@ class User < ActiveRecord::Base
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ?	BCrypt::Engine::MIN_COST: BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)	
+	end
+
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digested, User.digest(remember_token))		
 	end
 end
