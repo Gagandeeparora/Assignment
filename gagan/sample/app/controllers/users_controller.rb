5eb5795ffad20d8fea4b8fdde2e18defea24f8df
Gagandeep Arora <gagandeep.arora@letsgomo.com>
2015-04-06 18:03:20 +0530

    Sample App

--- gagan/sample/app/controllers/users_controller.rb
+++ gagan/sample/app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+
   before_action :logged_in_users, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
 	def show
 		@user = User.find(params[:id])		
 	end
@@ -33,15 +36,20 @@ class UsersController < ApplicationController
     end
   end
 
-  def logged_in_users
-    unless logged_in?
-      flash[:danger] = "Please Log in"
-      redirect_to login_url 
-    end
-  end
-
   private
    def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)  		
   	end
+
+    def logged_in_users
+    unless logged_in?
+      flash[:danger] = "Please Log in"
+      redirect_to login_url 
+    end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)      
+    end
+  end
 end
